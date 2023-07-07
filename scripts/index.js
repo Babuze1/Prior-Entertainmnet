8e1ce6b73ce3188cfb8bb8d848780bec65bfd088
Emmanuel <emwanzia532@gmail.com>
2023-07-07 22:07:00 +0300

    Completed assignment

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 //code is here
-function fetchData() {
-    console.log("START FETCH");
-}
-
-fetchData();
+document.addEventListener('DOMContentLoaded',function() {
+    async function fetchProducts(url) {
+        let data = await fetch(url);
+        let response = await data.json();
+        console.log(response);
+    };
+    fetchProducts('https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15')
+});
